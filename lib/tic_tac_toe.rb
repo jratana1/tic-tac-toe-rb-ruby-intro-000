cda8e591222a1fb564dc64b749bfc7d495019427
John Ratanaprasatporn <jratana1@gmail.com>
2020-09-11 19:46:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,3 +35,12 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  turn = 0
+  board.each do |element|
+    if element == 'X' || element == 'O'
+      turn =+ 1
+    end
+  end
+end 
